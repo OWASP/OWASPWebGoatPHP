c5a612a2f274e41535cc33cf06ae329c845a0fb3
Shivam Dixit <shivamd001@gmail.com>
2014-06-04 18:24:30 +0530

    Added template for lesson
    Signed-off-by: Shivam Dixit <shivamd001@gmail.com>

---
+++ template/SampleLesson/index.php
@@ -0,0 +1,101 @@
+<?php
+
+/**
+ * Copyright (c) 2014 Shivam Dixit <shivamd001@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * or (at your option) any later version, as published by the Free
+ * Software Foundation
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+namespace webgoat;
+
+/******************************************
+ * Template for creating a lesson         *
+ *                                        *
+ * Name of the directory of the lesson    *
+ * must be same as the name of the class  *
+ * in this file                           *
+ ******************************************/
+
+
+/**
+ * Main logic of the lesson
+ *
+ * Lesson Name: Sample Lesson
+ */
+class SampleLesson extends BaseLesson
+{
+    /**
+     * Get title of the lesson
+     *
+     * @return string Returns the title
+     */
+    public function getTitle()
+    {
+        return "Exploiting Hidden Fields";
+    }
+
+    /**
+     * Get hints of the lesson
+     *
+     * @return array Returns an array containing all the hints
+     */
+    public function getHints()
+    {
+        return $this->hints;
+    }
+
+    /**
+     * Get category of the lesson
+     *
+     * @return string Returns the lesson category
+     */
+    public function getCategory()
+    {
+        return "Sample Category";   //See category.php for list of all the categories
+    }
+
+    /**
+     * Starting point of the lesson
+     */
+    public function start()
+    {
+        $this->hints = array(
+            'Sample Hint 1',
+            'Sample Hint 2',
+            'Sample Hint 3'
+        );
+
+        $this->htmlContent .= file_get_contents(__DIR__."/content.html");
+
+        // If a form is submitted
+        // evaluate the submission
+        if (isset($_POST['submit'])) {
+
+            // If the submission is correct
+            $this->setCompleted(true);
+            $this->addSuccessMessage();
+        }
+    }
+
+    /**
+     * Reset the lesson
+     */
+    public function reset()
+    {
+        $this->setCompleted(false);
+        return true;
+    }
+}
