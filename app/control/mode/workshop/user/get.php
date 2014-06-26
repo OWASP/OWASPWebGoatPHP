91d0055e1346fe1965750e9c330f7062d8b7c5e9
Shivam Dixit <shivamd001@gmail.com>
2014-06-26 16:43:36 +0530

    Added a controller to get list of all workshop users
    Signed-off-by: Shivam Dixit <shivamd001@gmail.com>

---
+++ app/control/mode/workshop/user/get.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Copyright (c) 2014 Shivam Dixit <shivamd001@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * or (at your option) any later version, as published by the Free
+ * Software Foundation
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+class ModeWorkshopUserGetController extends JControl
+{
+    const PERMISSION_NAME = "delete_workshop_users";
+
+    public function Start()
+    {
+        $obj = new \webgoat\WorkshopUsers();
+        $users = $obj->getAll();
+
+        echo json_encode($users);
+        return true;
+    }
+}
