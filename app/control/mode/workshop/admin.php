c30b3abbd9f74aed2885f0131e2f1157c4937013
Shivam Dixit <shivamd001@gmail.com>
2014-06-27 23:20:33 +0530

    Fix for #14
    Signed-off-by: Shivam Dixit <shivamd001@gmail.com>

--- app/control/mode/workshop/admin.php
+++ app/control/mode/workshop/admin.php
@@ -4,43 +4,49 @@ class ModeWorkshopAdmin extends JControl
 {
     public function Start()
     {
-        if (jf::CurrentUser() && jf::Check('workshop')) {
-            $hiddenLessons = jf::LoadGeneralSetting("hiddenWorkshopLessons");
+        if (jf::CurrentUser()) {
+            if (jf::Check('workshop')) {
+                $hiddenLessons = jf::LoadGeneralSetting("hiddenWorkshopLessons");
 
-            // If request to hide the lesson
-            if (isset($_POST['hide'])) {
-                if ($hiddenLessons === null) {
-                    // If first request i.e settings not present
-                    $hiddenLessons = array($_POST['hide']);
-                } else {
-                    array_push($hiddenLessons, $_POST['hide']);
-                }
-
-                jf::SaveGeneralSetting("hiddenWorkshopLessons", $hiddenLessons);
-                echo json_encode(array('status' => true));
-                return true;
-            }
-
-            // If request to show the lesson
-            if (isset($_POST['show'])) {
-
-                if ($hiddenLessons !== null) {
-                    $position = array_search($_POST['show'], $hiddenLessons);
-                    if ($position !== false) {
-                        unset($hiddenLessons[$position]);
+                // If request to hide the lesson
+                if (isset($_POST['hide'])) {
+                    if ($hiddenLessons === null) {
+                        // If first request i.e settings not present
+                        $hiddenLessons = array($_POST['hide']);
+                    } else {
+                        array_push($hiddenLessons, $_POST['hide']);
                     }
+
+                    jf::SaveGeneralSetting("hiddenWorkshopLessons", $hiddenLessons);
+                    echo json_encode(array('status' => true));
+                    return true;
                 }
 
-                jf::SaveGeneralSetting("hiddenWorkshopLessons", $hiddenLessons);
-                echo json_encode(array('status' => true));
-                return true;
-            }
+                // If request to show the lesson
+                if (isset($_POST['show'])) {
 
-            // Get the list of all the lessons/categories
-            $this->allCategoryLesson = jf::LoadGeneralSetting("categoryLessons");
-            $this->hiddenLessons = jf::LoadGeneralSetting("hiddenWorkshopLessons");
-            return $this->Present();
+                    if ($hiddenLessons !== null) {
+                        $position = array_search($_POST['show'], $hiddenLessons);
+                        if ($position !== false) {
+                            unset($hiddenLessons[$position]);
+                        }
+                    }
+
+                    jf::SaveGeneralSetting("hiddenWorkshopLessons", $hiddenLessons);
+                    echo json_encode(array('status' => true));
+                    return true;
+                }
+
+                // Get the list of all the lessons/categories
+                $this->allCategoryLesson = jf::LoadGeneralSetting("categoryLessons");
+                $this->hiddenLessons = jf::LoadGeneralSetting("hiddenWorkshopLessons");
+                return $this->Present();
+            } else {
+                // User not authorized
+                $this->Redirect(SiteRoot);  // Redirect to home page instead of Login Page
+            }
         } else {
+            // User not authenticated
             $this->Redirect(jf::url()."/user/login?return=/".jf::$BaseRequest);
         }
     }
