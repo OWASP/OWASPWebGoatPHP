06151fc42d0a9f0b4f78f86c86c36a5ae7f91a17
Shivam Dixit <shivamd001@gmail.com>
2014-08-03 00:53:48 +0530

    Removed trailing spaces
    Signed-off-by: Shivam Dixit <shivamd001@gmail.com>

--- app/control/user/create.php
+++ app/control/user/create.php
@@ -2,33 +2,26 @@
 
 class UserCreateController extends JControl
 {
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
-	function __construct()
-	{
-		parent::__construct();
-	}
+    public function Start()
+    {
+        if (isset($_POST['username']) && isset($_POST['password'])) {
+            $Username = $_POST['username'];
+            $Password = $_POST['password'];
 
-	function Start()
-	{
-		if(isset($_POST['username']) && isset($_POST['password']))
-		{
-			$Username = $_POST['username'];
-			$Password = $_POST['password'];
+            if (jf::$User->UserExists($Username)) {
+                $this->Error = 1;
+            } else {
+                $result = jf::$User->CreateUser($Username, $Password);
+                $this->Result = true;
+                echo $result; //returns the userid of the user
+            }
+        }
 
-			if(jf::$User->UserExists($Username))
-			{
-				$this->Error = 1;
-			}
-			else
-			{
-				$result = jf::$User->CreateUser($Username, $Password);
-				$this->Result = true;
-				echo $result; //returns the userid of the user
-			}
-		}
-
-		return $this->Present();
-	}
+        return $this->Present();
+    }
 }
-
-?>
