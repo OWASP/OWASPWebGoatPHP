15f29bf3edca92f9f88ec761dfd76e9608eef11f
Shivam Dixit <shivamd001@gmail.com>
2014-08-04 10:48:50 +0530

    Added authentication
    Signed-off-by: Shivam Dixit <shivamd001@gmail.com>

--- app/control/mode/contest/home.php
+++ app/control/mode/contest/home.php
@@ -4,6 +4,19 @@ class ContestHomeController extends JControl
 {
     public function Start()
     {
-        return $this->Present();
+        $request = jf::$BaseRequest;
+
+        if (jf::CurrentUser()) {
+            // User is logged in, check if the user is authorized
+            if (jf::Check("view_contest_chal")) {
+                return $this->Present();
+            } else {
+                // User is not authorized
+                $this->Redirect(SiteRoot);
+            }
+        } else {
+            // User is not logged in
+            $this->Redirect(jf::url()."/user/login?return=/$request");
+        }
     }
 }
