768a2a2ad26af29f35282bb35de1f97b4ba037c5
Shivam Dixit <shivamd001@gmail.com>
2014-08-04 22:47:56 +0530

    Added favicon
    Signed-off-by: Shivam Dixit <shivamd001@gmail.com>

--- app/view/default/_template/head.php
+++ app/view/default/_template/head.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta name="author" content="Shivam Dixit">
+        <link rel="shortcut icon" type="image/png" href="<?php echo jf::url().'/images/favicon.ico'; ?>">
         <link rel="stylesheet" type="text/css" href="<?php echo jf::url().'/style/bootstrap.min.css'?>">
         <link rel="stylesheet" type="text/css" href="<?php echo jf::url().'/style/style.css'?>">
         <link rel="stylesheet" href="<?php echo jf::url()?>/style/reveal.css">
