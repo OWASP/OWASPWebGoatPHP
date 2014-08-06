a67136490cb3d2a001163f64b6e4d47065829dd3
Shivam Dixit <shivamd001@gmail.com>
2014-08-06 09:16:58 +0530

    Updated contest details model. Added method to check/get active contest. Signed-off-by: Shivam Dixit <shivamd001@gmail.com>

--- app/model/contest/details.php
+++ app/model/contest/details.php
@@ -36,15 +36,25 @@ class ContestDetails extends \JModel
      * Array must be of the form:
      * array('col_1' => val_1, 'col_2' => val_2, ...)
      *
+     * A new contest can be added only when the previous contest
+     * has finished.
+     *
      * @param $array Array key-value pairs to insert
      *
      * @return int Returns the insert ID (auto increment)
-     * @throws \Exception Required parameter missing
+     * @throws InvalidArgumentException If required parameter is missing,
+     * @throws ActiveContestPresentException If an active contest is present
      */
     public static function add($array = null)
     {
         if ($array === null || (count($array) <= 0)) {
-            throw new \Exception("Required parameter missing");
+            throw new InvalidArgumentException("Required parameter missing");
+        }
+
+        if (self::isActivePresent()) {
+            // Only one contest can be active at a time and since
+            // active contest is already present hence throw an exception
+            throw new ActiveContestPresentException("Active contest is already present");
         }
 
         $keys = array_keys($array);
@@ -66,9 +76,49 @@ class ContestDetails extends \JModel
     public static function getByID($id = null)
     {
         if ($id === null) {
-            throw new \Exception("Required parameter missing");
+            throw new \InvalidArgumentException("Required parameter missing");
         }
 
         return \jf::SQL("SELECT * FROM ".self::TABLE_NAME." WHERE ID = ?", $id);
+    }
+
+    /**
+     * Checks if an active contest is present.
+     * Each contest has two states:
+     * active -> When the contest has not finished
+     * inactive -> When the contest has finished
+     *
+     * For simplicity only one contest can be active at a time.
+     * This scheme is followed so that details of past contest
+     * are also available.
+     *
+     * @return bool True if active contest is present else false
+     */
+    public static function isActivePresent()
+    {
+        $currentTime = time();
+        $query = "SELECT * FROM ".self::TABLE_NAME." WHERE EndTimestamp > $currentTime";
+
+        $result = \jf::SQL($query);
+
+        if (count($result) == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Get the details of the active contest
+     *
+     * @return mixed Array containing details of active contest
+     * or null if no contest is active
+     */
+    public static function getActive()
+    {
+        $currentTime = time();
+        $query = "SELECT * FROM ".self::TABLE_NAME." WHERE EndTimestamp > $currentTime";
+
+        return \jf::SQL($query);
     }
 }
