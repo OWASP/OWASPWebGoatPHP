a0f026e2997d80a576c90b994e525c8dfbdba95d
Shivam Dixit <shivamd001@gmail.com>
2014-08-07 22:35:32 +0530

    Completed Add contest feature
    Signed-off-by: Shivam Dixit <shivamd001@gmail.com>

--- app/control/mode/contest/admin.php
+++ app/control/mode/contest/admin.php
@@ -6,6 +6,28 @@ class ModeContestAdmin extends JControl
     {
         if (jf::CurrentUser()) {
             if (jf::Check("contest")) {
+                // User is authorized
+
+                if (isset($_POST['contest_submit'])) {
+                    // Request to store the contest in the database
+                    $data = array(
+                        'ContestName' => $_POST['contest_name'],
+                        'ContestAdmin' => $_POST['contest_admin'],
+                        'StartTimestamp' => strtotime($_POST['start_date']),
+                        'EndTimestamp' => strtotime($_POST['end_date'])
+                    );
+
+                    \webgoat\ContestDetails::add($data);
+                }
+
+                if (\webgoat\ContestDetails::isActivePresent()) {
+                    // If an active contest is present
+
+                } else {
+                    // Show the option to start a contest
+                    $this->noActiveContest = true;
+                }
+
                 return $this->Present();
             } else {
                 // User is not authorized
--- app/view/default/mode/contest/admin.php
+++ app/view/default/mode/contest/admin.php
@@ -1,5 +1,21 @@
 <script src="<?php echo jf::url()."/script/contest-admin.js"?>"></script>
+<script src="<?php echo jf::url()."/script/moment.js"?>"></script>
+<script src="<?php echo jf::url()."/script/bootstrap-datetimepicker.js"?>"></script>
 <link rel="stylesheet" type="text/css" href="<?php echo jf::url().'/style/dashboard.css'?>">
+<link rel="stylesheet" type="text/css" href="<?php echo jf::url().'/style/bootstrap-datetimepicker.css'?>">
+
+<script type="text/javascript">
+    $(function() {
+        // Initialize the date time picker
+        $('#start-date-time').datetimepicker({
+            language: 'en'
+        });
+        $('#end-date-time').datetimepicker({
+            language: 'en'
+        });
+    });
+</script>
+
 <!--navbar
 ============-->
 <div class="navbar navbar-inverse navbar-fixed-top">
@@ -43,6 +59,77 @@
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
             <h1 class="page-header" id="heading"></h1>
             <div id="main-content">
+                <!-----------------OVERVIEW----------------->
+                <div id="overview-content" class="hidden">
+                    <?php if (isset($this->noActiveContest)):?>
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <div class="alert alert-danger">
+                                    There is no active contest. <strong>Start one!</strong>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <h4>Enter contest details</h4>
+                            </div>
+                        </div>
+                        <br>
+                        <form role="form" method="POST">
+                            <div class="form-group">
+                                <label for="contest-name">Contest Name</label>
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <input type="text" class="form-control" id="contest-name"
+                                               placeholder="Enter name" name="contest_name">
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="contest-admin">Contest admin</label>
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <input type="text" class="form-control" id="contest-admin"
+                                               placeholder="Enter name" name="contest_admin">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="row">
+                                    <div class="col-sm-3">
+                                        <label for="start-date-time">Start date</label>
+                                        <div id="start-date-time" class="input-group date">
+                                            <div class="input-group">
+                                                <input data-format="DD/MM/YYYY hh:mm a" type="text"
+                                                       class="form-control" name="start_date">
+                                                <div class="input-group-addon">
+                                                    <span class="glyphicon glyphicon-calendar"></span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-3">
+                                        <label for="end-date-time">End date</label>
+                                        <div id="end-date-time" class="input-group date">
+                                            <div class="input-group">
+                                                <input data-format="DD/MM/YYYY hh:mm a" type="text"
+                                                       class="form-control" name="end_date">
+                                                <div class="input-group-addon">
+                                                    <span class="glyphicon glyphicon-calendar"></span>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <br>
+                            <input type="submit" class="btn btn-success btn-lg" name="contest_submit" value="Submit">
+                        </form>
+                    <?php endif;?>
+                </div>
+
             </div><!--End main-content-->
 
         </div>
